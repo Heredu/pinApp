fe42f79e1921fced52efcb185859920cf8025f0f
heredu <heredu_romero@hotmail.com>
2025-06-25 19:11:31 -0300

    redis

--- src/main/java/com/pinApp/customerManagement/CustomerManagementApplication.java
+++ src/main/java/com/pinApp/customerManagement/CustomerManagementApplication.java
@@ -8,8 +8,6 @@ public class CustomerManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerManagementApplication.class, args);
-		System.out.println("SPRING_RABBITMQ_HOST: " + System.getenv("SPRING_RABBITMQ_HOST"));
-		System.out.println("RABBITMQ_HOST (Render): " + System.getenv("RABBITMQ_HOST"));
 	}
 
 }
