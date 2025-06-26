2003da42e1b92907977a746c94bc128a91549b97
heredu <heredu_romero@hotmail.com>
2025-06-25 21:17:07 -0300

    config

--- src/main/java/com/pinApp/customerManagement/mq/config/RedisConfig.java
+++ src/main/java/com/pinApp/customerManagement/mq/config/RedisConfig.java
@@ -1,17 +1,34 @@
 package com.pinApp.customerManagement.mq.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+
+
+        return new LettuceConnectionFactory(config);
+    }
+
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
-        StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(connectionFactory);
-        return template;
+        return new StringRedisTemplate(connectionFactory);
     }
 }
